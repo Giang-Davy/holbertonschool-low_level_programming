6b95b6a25ef71548d94384c517f8cb2f402415ff
Giang-Davy <davy.dangvu@gmail.com>
2024-11-28 20:22:55 +0000

    ajout 2

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,18 @@
 #include "main.h"
 
 /**
- *_strlen(char *str) - c ff
+ *_strlen - c ff
  *@s: c ff
  *
+ * Return: ff
  **/
+int _strlen(char *s)
+{
+	int length = 0;
 
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
