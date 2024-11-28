fb3a40a2e72c76e932626207dab9cf8e060faedf
Giang-Davy <davy.dangvu@gmail.com>
2024-11-28 23:18:59 +0000

    100V2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,40 +1,24 @@
-#include "main.h"
-
-/**
- * _atoi - Convertit une chaîne en entier
- * @s: La chaîne à convertir
- *
- * Return: L'entier converti
- */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	unsigned int num = 0;
+    int result = 0;
+    int sign = 1;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == ' ')
-		{
-			i++;
-		}
-		else
-		{
-			break;
-		}
-	}
-	while (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		i++;
-	}
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
-		i++;
-	}
-	return (num * sign);
+    /* Skip leading non-numeric characters */
+    while (*s && (*s < '0' || *s > '9'))
+    {
+        if (*s == '-')
+        {
+            sign *= -1;
+        }
+        s++;
+    }
+
+    /* Convert string to integer */
+    while (*s >= '0' && *s <= '9')
+    {
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+
+    return result * sign;
 }
