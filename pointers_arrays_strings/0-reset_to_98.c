71e74659a4af065e728d89e07a7bd2038e2d448d
Giang-Davy <davy.dangvu@gmail.com>
2024-11-28 14:07:09 +0000

    0-V14

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,10 +3,10 @@
 /**
  * reset_to_98 - Met |  jour la valeur pointée par n |  98.
  * @n: Un pointeur vers un entier.
- *
  * Cette fonction prend un pointeur vers un entier et modifie la valeur
  * de l'entier pointé pour la rendr
-**/void reset_to_98(int *n)
+**/
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
