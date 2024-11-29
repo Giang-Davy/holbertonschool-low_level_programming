58ea2223fdd4f3e7aeca5b089882dcdb83ab0ed5
Giang-Davy <davy.dangvu@gmail.com>
2024-11-29 11:09:56 +0000

    8V2

--- doubly_linked_lists/0-main.c
+++ doubly_linked_lists/0-main.c
@@ -27,7 +27,7 @@ int main(void)
     new->next = head;
     new->prev = NULL;
     head = new;
-    n = print_dlistint(head);
+    print_dlistint(head);
     printf("-> %lu elements\n", n);
     free(new);
     return (EXIT_SUCCESS);
